f5dfedd40ff17fa40e8dd157e8c81de900678b1b
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-17 01:14:55 -0300

    refactor: enable view binding to activities

--- app/src/main/java/com/example/petlife/EditPetActivity.kt
+++ app/src/main/java/com/example/petlife/EditPetActivity.kt
@@ -5,16 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.petlife.databinding.ActivityEditPetBinding
 
 class EditPetActivity : AppCompatActivity() {
+    private val apb: ActivityEditPetBinding by lazy {
+        ActivityEditPetBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_pet)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(apb.root)
+
     }
 }
