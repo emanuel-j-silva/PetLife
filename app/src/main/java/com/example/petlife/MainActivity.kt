1558c4b1811c4a75ff59c1ca6199762988a0873f
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-19 09:46:29 -0300

    feat: implement activity result launcher for edit pet

--- app/src/main/java/com/example/petlife/MainActivity.kt
+++ app/src/main/java/com/example/petlife/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.petlife
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.petlife.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +12,28 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private lateinit var parl:ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
 
+        parl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == RESULT_OK){
+                result.data?.let { data->
+                    val name = data.getStringExtra("name") ?: ""
+                    val birthDate = data.getStringExtra("birthDate") ?: ""
+                    val type = data.getStringExtra("type") ?: ""
+                    val color = data.getStringExtra("color") ?: ""
+                    val size = data.getStringExtra("size") ?: ""
+
+                    amb.nameTv.text = name
+                    amb.birthTv.text = birthDate
+                    amb.typeTv.text = type
+                    amb.colorTv.text = color
+                    amb.sizeTv.text = size
+                }
+            }
+        }
     }
 }
