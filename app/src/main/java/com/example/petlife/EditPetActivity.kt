fda11343e4f81a2a9822b94008b4d26ffe439f18
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-19 11:31:43 -0300

    chore: remove unnecessary imports

--- app/src/main/java/com/example/petlife/EditPetActivity.kt
+++ app/src/main/java/com/example/petlife/EditPetActivity.kt
@@ -4,10 +4,7 @@ import android.R
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.petlife.databinding.ActivityEditPetBinding
 
 class EditPetActivity : AppCompatActivity() {
