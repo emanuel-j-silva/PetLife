9c25a20151db7366fd67daa5eaf5e0e445130382
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-19 09:54:49 -0300

    fix: remove unnecessary parcelize import

--- app/src/main/java/com/example/petlife/Pet.kt
+++ app/src/main/java/com/example/petlife/Pet.kt
@@ -1,13 +1,9 @@
 package com.example.petlife
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Pet(
     val name: String,
     val birthDate: String,
     val type: Type,
     val color: String,
     val size: Size,
-) : Parcelable
+)
