5b07d4dfa7c93c8d5f43d83f576b1a9a88d975fb
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-19 11:10:59 -0300

    feat: add some logic to edit pet activity

--- app/src/main/java/com/example/petlife/EditPetActivity.kt
+++ app/src/main/java/com/example/petlife/EditPetActivity.kt
@@ -1,6 +1,9 @@
 package com.example.petlife
 
+import android.R
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,9 +14,67 @@ class EditPetActivity : AppCompatActivity() {
     private val apb: ActivityEditPetBinding by lazy {
         ActivityEditPetBinding.inflate(layoutInflater)
     }
+
+    private var pet: Pet? = null
+
+    private lateinit var typeAdapter: ArrayAdapter<Type>
+    private lateinit var sizeAdapter: ArrayAdapter<Size>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(apb.root)
 
+        typeAdapter = ArrayAdapter(this, R.layout.simple_spinner_item,
+            Type.entries)
+        typeAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        apb.typeSp.adapter = typeAdapter
+
+        sizeAdapter = ArrayAdapter(this, R.layout.simple_spinner_item,
+            Size.entries)
+        sizeAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        apb.sizeSp.adapter = sizeAdapter
+
+        pet = Pet(
+            name = intent.getStringExtra("name") ?: "",
+            birthDate = intent.getStringExtra("birthDate") ?: "",
+            type = intent.getStringExtra("type")?.let { Type.valueOf(it) } ?: Type.DOG,
+            color = intent.getStringExtra("color") ?: "",
+            size = intent.getStringExtra("size")?.let { Size.valueOf(it) } ?:Size.MEDIUM
+        )
+
+        pet?.let { fillEditFields(it) }
+
+        apb.saveBt.setOnClickListener{
+            pet = Pet(
+                name = apb.nameEt.text.toString(),
+                birthDate = apb.birthEt.text.toString(),
+                type = Type.valueOf(apb.typeSp.selectedItem.toString()),
+                color = apb.colorEt.text.toString(),
+                size = Size.valueOf(apb.sizeSp.selectedItem.toString())
+            )
+
+            val resultIntent = Intent().apply {
+                putExtra("name", pet?.name)
+                putExtra("birthDate", pet?.birthDate)
+                putExtra("type", pet?.type?.name)
+                putExtra("color", pet?.color)
+                putExtra("size", pet?.size?.name)
+            }
+            setResult(RESULT_OK,resultIntent)
+            finish()
+        }
+
+    }
+    private fun fillEditFields(pet: Pet){
+        apb.nameEt.setText(pet.name)
+        apb.birthEt.setText(pet.birthDate)
+
+        val typePosition = typeAdapter.getPosition(pet.type)
+        apb.typeSp.setSelection(typePosition)
+
+        apb.colorEt.setText(pet.color)
+
+        val sizePosition = sizeAdapter.getPosition(pet.size)
+        apb.sizeSp.setSelection(sizePosition)
     }
 }
