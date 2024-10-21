1d157ccbf5dbb0ed694e1661c3049350fe3e30eb
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-21 01:00:24 -0300

    feat: create last vaccination edit activity

---
+++ app/src/main/java/com/example/petlife/LastVacActivity.kt
@@ -0,0 +1,35 @@
+package com.example.petlife
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.petlife.databinding.ActivityLastVacBinding
+
+class LastVacActivity : AppCompatActivity() {
+    private val alb: ActivityLastVacBinding by lazy {
+        ActivityLastVacBinding.inflate(layoutInflater)
+    }
+    private var lastVaccination: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(alb.root)
+
+        lastVaccination = intent.getStringExtra("lastVaccination")
+        lastVaccination?.let { fillEditField(it) }
+
+        alb.saveBt.setOnClickListener{
+            lastVaccination = alb.textTv.text.toString()
+
+            val resultIntent = Intent().apply {
+                putExtra("lastVaccination",lastVaccination)
+            }
+            setResult(RESULT_OK,resultIntent)
+            finish()
+        }
+    }
+
+    private fun fillEditField(string: String){
+        alb.textTv.setText(string)
+    }
+}
